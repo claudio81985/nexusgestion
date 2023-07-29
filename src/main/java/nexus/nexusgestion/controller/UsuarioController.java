1e967416ca4791a6d9b7243332ff7a625ca93638
zanazzoalan <alanzanazzo44@gmail.com>
2023-07-29 19:53:58 -0300

    corregido vista Ventas + registros de pruebas en la BD

--- src/main/java/nexus/nexusgestion/controller/UsuarioController.java
+++ src/main/java/nexus/nexusgestion/controller/UsuarioController.java
@@ -1,116 +1,116 @@
-package nexus.nexusgestion.controller;
+// package nexus.nexusgestion.controller;
 
 
-import java.util.List;
+// import java.util.List;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
-// import org.springframework.security.access.annotation.Secured;
+// import org.springframework.beans.factory.annotation.Autowired;
 // import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.bind.support.SessionStatus;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+// // import org.springframework.security.access.annotation.Secured;
+// // import org.springframework.security.crypto.password.PasswordEncoder;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.ui.Model;
+// import org.springframework.validation.BindingResult;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.ModelAttribute;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.SessionAttributes;
+// import org.springframework.web.bind.support.SessionStatus;
+// import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import nexus.nexusgestion.Model.Entities.Permiso;
-import nexus.nexusgestion.Model.Entities.Usuario;
-import nexus.nexusgestion.Model.Service.IPermisoService;
-import nexus.nexusgestion.Model.Service.IUsuarioService;
+// import nexus.nexusgestion.Model.Entities.Permiso;
+// import nexus.nexusgestion.Model.Entities.Usuario;
+// import nexus.nexusgestion.Model.Service.IPermisoService;
+// import nexus.nexusgestion.Model.Service.IUsuarioService;
 
-import javax.validation.Valid;
+// import javax.validation.Valid;
 
-@Controller
-@RequestMapping("/usuarios")
-@SessionAttributes("usuario")
-// @Secured({"ROLE_ADMIN", "ROLE_USER"})
-public class UsuarioController {
+// @Controller
+// @RequestMapping("/usuarios")
+// @SessionAttributes("usuario")
+// // @Secured({"ROLE_ADMIN", "ROLE_USER"})
+// public class UsuarioController {
 
-    @Autowired
-     IUsuarioService usuarioService;
+//     @Autowired
+//      IUsuarioService usuarioService;
 
-    @Autowired
-    IPermisoService permisoService;
+//     @Autowired
+//     IPermisoService permisoService;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+//     @Autowired
+//     private PasswordEncoder passwordEncoder;
 
-    @GetMapping("/listado")
-    public String listado(Model model) {
+//     @GetMapping("/listado")
+//     public String listado(Model model) {
 
-        model.addAttribute("titulo", "Listado de Usuarios");
-        model.addAttribute("usuarios", usuarioService.buscarTodos());
+//         model.addAttribute("titulo", "Listado de Usuarios");
+//         model.addAttribute("usuarios", usuarioService.buscarTodos());
 
-        return "usuarios/list";
+//         return "usuarios/list";
 
-    }
+//     }
 
-    @GetMapping("/nuevo")
-    public String nuevo(Model model) {
+//     @GetMapping("/nuevo")
+//     public String nuevo(Model model) {
 
-        model.addAttribute("titulo", "Nuevo Usuario");
-        model.addAttribute("usuario", new Usuario());
-        model.addAttribute("permisos", permisoService.buscarTodo());
+//         model.addAttribute("titulo", "Nuevo Usuario");
+//         model.addAttribute("usuario", new Usuario());
+//         model.addAttribute("permisos", permisoService.buscarTodo());
 
-        return "usuarios/form";
-    }
+//         return "usuarios/form";
+//     }
 
-    @PostMapping("/guardar")
-    public String guardar(@Valid Usuario usuario, BindingResult result, @RequestParam("rol") Long idRol,
-            Model model, RedirectAttributes msgFlash, SessionStatus status) {
+//     @PostMapping("/guardar")
+//     public String guardar(@Valid Usuario usuario, BindingResult result, @RequestParam("rol") Long idRol,
+//             Model model, RedirectAttributes msgFlash, SessionStatus status) {
 
-        // Verificar si hay errores
-        if (result.hasErrors()) {
-            model.addAttribute("danger", "Corrija los Errores...");
-            return "usuarios/form";
-        }
+//         // Verificar si hay errores
+//         if (result.hasErrors()) {
+//             model.addAttribute("danger", "Corrija los Errores...");
+//             return "usuarios/form";
+//         }
 
-        usuario.setPermiso(permisoService.buscarPorId(idRol));
+//         usuario.setPermiso(permisoService.buscarPorId(idRol));
         
-        //if(usuario.getId() == 0)
-            usuario.setClave(passwordEncoder.encode(usuario.getClave()));
+//         //if(usuario.getId() == 0)
+//             usuario.setClave(passwordEncoder.encode(usuario.getClave()));
         
-            usuarioService.guardar(usuario);
+//             usuarioService.guardar(usuario);
 
-        msgFlash.addFlashAttribute("success", "Usuario Registrado Correctamente.");
-        status.setComplete();
+//         msgFlash.addFlashAttribute("success", "Usuario Registrado Correctamente.");
+//         status.setComplete();
 
-        return "redirect:/usuarios/listado";
-    }
+//         return "redirect:/usuarios/listado";
+//     }
 
-    @GetMapping("/borrar/{id}")
-    public String deshabOrHabUsuario(@PathVariable("id") Long id, RedirectAttributes msgFlash) {
+//     @GetMapping("/borrar/{id}")
+//     public String deshabOrHabUsuario(@PathVariable("id") Long id, RedirectAttributes msgFlash) {
 
-        Usuario usuario = usuarioService.buscarPorId(id);
-        usuario.setActivo(!usuario.isActivo());
-        usuarioService.guardar(usuario);
-        msgFlash.addFlashAttribute("warning", usuario.isActivo() ? "Usuario Habilitado" : "Usuario Deshabilitado");
+//         Usuario usuario = usuarioService.buscarPorId(id);
+//         usuario.setActivo(!usuario.isActivo());
+//         usuarioService.guardar(usuario);
+//         msgFlash.addFlashAttribute("warning", usuario.isActivo() ? "Usuario Habilitado" : "Usuario Deshabilitado");
 
-        return "redirect:/usuarios/listado";
-    }
+//         return "redirect:/usuarios/listado";
+//     }
 
-    @GetMapping("/editar/{id}")
-    public String editar(@PathVariable("id") Long id, Model model) {
+//     @GetMapping("/editar/{id}")
+//     public String editar(@PathVariable("id") Long id, Model model) {
 
-        Usuario usuario = usuarioService.buscarPorId(id);
+//         Usuario usuario = usuarioService.buscarPorId(id);
 
-        model.addAttribute("titulo", "Nuevo Usuario");
-        model.addAttribute("usuario", usuario);
+//         model.addAttribute("titulo", "Nuevo Usuario");
+//         model.addAttribute("usuario", usuario);
 
-        return "usuarios/form";
-    }
+//         return "usuarios/form";
+//     }
 
-    @ModelAttribute("permisos")
-    public List<Permiso> getPermisos() {
-        return permisoService.buscarTodo();
-    }
+//     @ModelAttribute("permisos")
+//     public List<Permiso> getPermisos() {
+//         return permisoService.buscarTodo();
+//     }
 
-}
+// }
