9dc2bd7ca120f7b8970725dec9f28614877545be
zanazzoalan <alanzanazzo44@gmail.com>
2023-07-31 10:55:39 -0300

    Barra de busqueda funcionando

--- src/main/resources/static/js/inventario.js
+++ src/main/resources/static/js/inventario.js
@@ -4,3 +4,22 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 
+
+// BUSQUEDA DE ARTICULOS
+document.addEventListener("DOMContentLoaded", function () {
+  const searchBar = document.getElementById("search-bar");
+  const searchInput = searchBar.querySelector("input");
+  const tableRows = document.querySelectorAll("tbody tr");
+
+  searchInput.addEventListener("input", function () {
+    const searchTerm = this.value.trim().toLowerCase();
+
+    tableRows.forEach(function (row) {
+      const articleName = row.cells[3].innerText.toLowerCase();
+      const articleCode = row.cells[1].innerText.toLowerCase();
+      const showRow = articleName.includes(searchTerm) || articleCode.includes(searchTerm);
+      row.style.display = showRow ? "" : "none";
+    });
+  });
+});
+
---
+++ src/main/resources/static/js/proveedores.js
@@ -0,0 +1,25 @@
+// Tooltip
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+
+// BUSQUEDA DE PROVEEDORES
+document.addEventListener("DOMContentLoaded", function () {
+    const searchBar = document.getElementById("search-bar");
+    const searchInput = searchBar.querySelector("input");
+    const tableRows = document.querySelectorAll("tbody tr");
+  
+    searchInput.addEventListener("input", function () {
+      const searchTerm = this.value.trim().toLowerCase();
+  
+      tableRows.forEach(function (row) {
+        const providerName = row.cells[2].innerText.toLowerCase();
+        const providerCUIT = row.cells[1].innerText.toLowerCase();
+        const showRow = providerName.includes(searchTerm) || providerCUIT.includes(searchTerm);
+        row.style.display = showRow ? "" : "none";
+      });
+    });
+  });
+  
