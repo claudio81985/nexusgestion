b99bbb4075750788a5cbc0f8441fe56057026605
zanazzoalan <alanzanazzo44@gmail.com>
2023-09-17 19:42:01 -0300

    corrección en precios mostrados precios

--- src/main/resources/static/js/compraForm.js
+++ src/main/resources/static/js/compraForm.js
@@ -4,39 +4,16 @@ let listaProductos;
 const lineasUtil = {
   incrementarCantidad: function (id, precio) {
     let cantidad = parseInt($(`#cantidad_${id}`).val());
-    console.log(`Cantidad = ${cantidad}`);
     $(`#cantidad_${id}`).val(++cantidad);
     this.calcularSubtotal(id, precio, cantidad);
   },
 
   calcularSubtotal: function (id, precio, cantidad) {
-    console.log(`Contenido de 'id'=${id}`);
     let stk = listaProductos.find((i) => i.id === id); // Busca el stock que coincide con el id del producto
-    console.log(`Contenido de 'stk'=${stk}`);
-    console.log(`stk.stockSucursalUno = ${stk.stockSucursalUno}`);
-    console.log(`stk.stockSucursalDos = ${stk.stockSucursalDos}`);
-    
-    // if (sucursalUsuario === 1 && cantidad > stk.stockSucursalUno) {
-    //   Swal.fire({
-    //     icon: "error",
-    //     title: "Oops...",
-    //     text: "No hay stock suficiente en Sucursal Sauzalito!",
-    //   });
-    //   $(`#cantidad_${id}`).val(stk.stockSucursalUno);
-    // } else if (sucursalUsuario === 2 && cantidad > stk.stockSucursalDos) {
-    //   Swal.fire({
-    //     icon: "error",
-    //     title: "Oops...",
-    //     text: "No hay stock suficiente en Sucursal Fontana!",
-    //   });
-    //   $(`#cantidad_${id}`).val(stk.stockSucursalDos);
-    // } else {
-      console.log(`Cantidad = ${cantidad}`);
       $(`#subtotal_${id}`).html(
         (parseFloat(precio) * parseInt(cantidad)).toFixed(2)
       );
       this.calcularTotal();
-    // }
   },
   
 
@@ -55,7 +32,6 @@ const lineasUtil = {
     $(`span[id^="subtotal_"]`).each(function () {
       total += parseFloat($(this).html());
     });
-    console.log("Total = ", total);
     $("#total").html("$" + parseFloat(total).toFixed(2));
   },
 
@@ -72,11 +48,6 @@ $(document).ready(function () {
       dataType: "json",
       success: function (data) {
         sucursalUsuario = data;
-        console.log("usuario:", data);
-        if (data = 2) {
-          console.log("Sucursal usuario = Sucursal Fontana");
-        }
-
         $("#buscar_productos").autocomplete({
           minLength: 3,
           source: (request, response) => {
@@ -87,7 +58,6 @@ $(document).ready(function () {
                 term: request.term,
               },
               success: (data) => {
-                console.log("Datos recibidos:", data);
                 listaProductos = data;
                 response(
                   $.map(data, (item) => {
@@ -116,7 +86,7 @@ $(document).ready(function () {
 
                     return {
                       value: item.id,
-                      label: `[${item.codigoIdentificacion}] ${item.nombreComun} ${item.nombreTecnico} ${item.descripcion} - $${item.precio}`,
+                      label: `[${item.codigoIdentificacion}] ${item.nombreComun} ${item.nombreTecnico} ${item.descripcion} - $${item.precioCompra}`,
                     };
                   })
                 );
@@ -129,14 +99,12 @@ $(document).ready(function () {
 
             //Asignar valores a sus celdas
             let producto = ui.item.label;
-            let descripcion = producto.split("]")[1].trim(); //let descripcion = producto.split('-')[0];
+            let descripcion = producto.split("]")[1].trim();
             descripcion = descripcion.split("-")[0];
             let precio = producto.split("-")[1];
             precio = precio.split("$")[1];
             let id = ui.item.value;
             let codigoIdentificacion = producto.split("[")[1].split("]")[0];
-
-            // console.log(`Producto seleccionado: ${producto}`);
 
             //Verificar si es repetido el producto...
             if (lineasUtil.esRepetido(id)) {
@@ -151,9 +119,6 @@ $(document).ready(function () {
             linea = linea.replace(/{PRECIO}/g, precio);
 
             $("#tabla_productos tbody").append(linea);
-
-            // console.log(`id del producto antes de calcularSubtotal ${id}`);
-            // console.log(`precio del producto antes de calcularSubtotal ${precio}`);
             lineasUtil.calcularSubtotal(id, precio, 1);
           },
         });
